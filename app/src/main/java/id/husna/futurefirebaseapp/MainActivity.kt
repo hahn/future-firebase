d5e789ee4bce72cc678d65b1d696e46a5920c723
Hanhan Husna Firmansyah <hanhan.firmansyah@gdn-commerce.com>
2021-09-21 09:58:38 +0700

    fcm notification

--- app/src/main/java/id/husna/futurefirebaseapp/MainActivity.kt
+++ app/src/main/java/id/husna/futurefirebaseapp/MainActivity.kt
@@ -2,10 +2,38 @@ package id.husna.futurefirebaseapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.installations.FirebaseInstallations
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
+  private val TAG = "FirebaseApp"
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    getToken()
+  }
+
+  private fun getToken() {
+    FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+      if (!task.isSuccessful) {
+        Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+        return@addOnCompleteListener
+      }
+
+      val token = task.result
+      Log.d(TAG, "message token: $token")
+      Toast.makeText(this, "Token: $token", Toast.LENGTH_LONG).show()
+    }
+
+    FirebaseInstallations.getInstance().id.addOnCompleteListener { installationIdTask ->
+      if (installationIdTask.isSuccessful) {
+        Log.d(TAG, "Firebase Installations ID: ${installationIdTask.result}")
+      } else {
+        Log.e(TAG, "Unable to retrieve installations ID",
+          installationIdTask.exception)
+      }
+    }
   }
 }
